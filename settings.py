2a4cdc8ff25814265ab2b46c41f7c9103127a3e0
CatherineKatherine <110198127+CatherineKatherine@users.noreply.github.com>
2023-09-02 19:59:56 +0300

    Создание настроек для направления флота

--- settings.py
+++ settings.py
@@ -20,5 +20,8 @@ class Settings:
 
         # Настройки пришельцев
         self.alien_speed = 1.0
+        self.fleet_drop_speed = 10
+        # fleet_direction = 1 - движение вправо; -1 - влево
+        self.fleet_drop_speed = 1
 
 
