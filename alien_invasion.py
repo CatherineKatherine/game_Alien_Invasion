e45037cc478a7ef3d2b5ffb6828f37d2daed8222
CatherineKatherine <110198127+CatherineKatherine@users.noreply.github.com>
2023-09-12 21:47:00 +0300

    Начисление очков за все попадания

--- alien_invasion.py
+++ alien_invasion.py
@@ -148,7 +148,8 @@ class AlienInvasion:
             self.bullets, self.aliens, True, True)
 
         if collisions:
-            self.stats.score += self.settings.alien_points
+            for alien in collisions.values():
+                self.stats.score += self.settings.alien_points * len(alien)
             self.sb.prep_score()
 
         if not self.aliens:
